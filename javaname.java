a0f1d6b0935dabedb7baa7d7c95a7ecfb3f55826
BryantCR <bryancvargas20@gmail.com>
2021-10-18 11:50:21 -0600

    All working good

---
+++ javaname.java
@@ -0,0 +1,11 @@
+// My name is Bryan
+// I am 19 years old
+// My hometown is San José, CR
+
+public class javaname {
+    public static void main(String[] args) {
+        System.out.println("My name is Bryan");
+        System.out.println("I am 19 years old");
+        System.out.println("My hometown is San José, CR");
+    }
+}
